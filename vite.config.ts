a66296686f028a6676db85966d5c81f63ce993d5
FernandoGv <fernnandovilagrand@gmail.com>
2025-11-24 14:27:32 -0300

    ajuste Sismap

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://starmotmotor.com.br', // coloque o seu domínio
+      dynamicRoutes: [
+        '/',       // rota Home
+        '/sobre'   // rota Sobre
+      ],
+      generateRobotsTxt: true
+    })
   ],
 })
